48568336f6717111611e06a646738ae0e19f9ed7
ellw <blu3tim3@gmail.com>
2015-02-03 12:17:16 +0100

    Update

--- src/InterLeave.java
+++ src/InterLeave.java
@@ -1,29 +1,41 @@
 import java.util.Arrays;
 public class InterLeave {
-    public static int[] interleave(int[] werte1, int[] werte2) {
-        int[] array = new int[werte1.length+werte2.length];
-        if (werte1.length != werte2.length) {
+    public static int[] interleave(int[] arrEins, int[] arrZwei) {
+        if (arrEins.length != arrZwei.length) {
             return null;
-        } else {
-            int count1 = 0;
-            int count2 = 0;
-            for (int i = 0; i < array.length; i++){
-                if (i%2 == 0){
-                    array[i] = werte1[count1];
-                    count1++;
-                } else {
-                    array[i] = werte2[count2];
-                    count2++;
-                }
-            }
-            return array;
         }
+        int[] result = new int[arrEins.length + arrZwei.length];
+        int countA = 0, countB = 0;
+        for (int i = 0; i < result.length; i++) {
+            if (i % 2 == 0) {
+                result[i] = arrEins[countA++];
+            } else {
+                result[i] = arrZwei[countB++];
+            }
+        }
+        return result;
     }
+
+    public static int[] interleave2(int[] arrEins, int[] arrZwei) {
+        int[] result = new int[arrEins.length + arrZwei.length];
+        if (arrEins.length == arrZwei.length) {
+            for (int i = 0; i < arrEins.length; i++) {
+                result[2 * i] = arrEins[i];
+                result[(2 * i) + 1] = arrZwei[i];
+            }
+        } else {
+            return null;
+        }
+        return result;
+    }
+
     public static void main(String[] args){
-        int[] werte1 = {1,2,3,4};
-        int[] werte2 = {5,6,7,8};
-        int[] werteTest = {0};
-        System.out.println(Arrays.toString(interleave(werte1, werte2)));
-        System.out.println(Arrays.toString(interleave(werte1, werteTest)));
+        int[] test4 = {0, 1, 2, 1, 0};
+        int[] test5 = {0, 1, 2};
+        int[] test6 = {3, 4, 5};
+        System.out.println(Arrays.toString(interleave(test5, test6)));
+        System.out.println(Arrays.toString(interleave(test4, test6)));
+        System.out.println(Arrays.toString(interleave2(test5, test6)));
+        System.out.println(Arrays.toString(interleave2(test4, test6)));
     }
 }
--- src/TestingEnvironment.java
+++ src/TestingEnvironment.java
@@ -43,8 +43,8 @@ public class TestingEnvironment {
 
     public static boolean isPalindrom(int[] array) {
         boolean result = false;
-        for(int i = 0; i < array.length/2; i++){
-            if(array[i]==array[(array.length-1)-i]){
+        for (int i = 0; i < array.length / 2; i++) {
+            if (array[i] == array[(array.length - 1) - i]) {
                 result = true;
             } else {
                 result = false;
@@ -54,22 +54,34 @@ public class TestingEnvironment {
         return result;
     }
 
-    public static int[] interleave(int[] arrEins, int[] arrZwei){
-        if(arrEins.length!=arrZwei.length){
+    public static int[] interleave(int[] arrEins, int[] arrZwei) {
+        if (arrEins.length != arrZwei.length) {
             return null;
         }
-        int[] result = new int[arrEins.length+arrZwei.length];
+        int[] result = new int[arrEins.length + arrZwei.length];
         int countA = 0, countB = 0;
-        for(int i = 0; i < result.length; i++){
-            if(i%2 == 0){
-                result[i]=arrEins[countA++];
+        for (int i = 0; i < result.length; i++) {
+            if (i % 2 == 0) {
+                result[i] = arrEins[countA++];
             } else {
-                result[i]=arrZwei[countB++];
+                result[i] = arrZwei[countB++];
             }
         }
         return result;
     }
 
+    public static int[] interleave2(int[] arrEins, int[] arrZwei) {
+        int[] result = new int[arrEins.length + arrZwei.length];
+        if (arrEins.length == arrZwei.length) {
+            for (int i = 0; i < arrEins.length; i++) {
+                result[2 * i] = arrEins[i];
+                result[(2 * i) + 1] = arrZwei[i];
+            }
+        } else {
+            return null;
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
         System.out.println(istQuadratzahl(0));
@@ -89,9 +101,11 @@ public class TestingEnvironment {
         System.out.println(isPalindrom(test3));
         System.out.println(isPalindrom(test4));
 
-        int[] test5 = {0,1,2};
-        int[] test6 = {3,4,5};
-        System.out.println(Arrays.toString(interleave(test5,test6)));
-        System.out.println(Arrays.toString(interleave(test4,test6)));
+        int[] test5 = {0, 1, 2};
+        int[] test6 = {3, 4, 5};
+        System.out.println(Arrays.toString(interleave(test5, test6)));
+        System.out.println(Arrays.toString(interleave(test4, test6)));
+        System.out.println(Arrays.toString(interleave2(test5, test6)));
+        System.out.println(Arrays.toString(interleave2(test4, test6)));
     }
 }
