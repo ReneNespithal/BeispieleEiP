61e47bfd2de82902da89c98a6ec374047d1dd268
ellw <blu3tim3@gmail.com>
2015-02-04 14:46:15 +0100

    Update

--- src/testing/Rekursiv.java
+++ src/testing/Rekursiv.java
@@ -1,32 +1,31 @@
 package testing;
 
-/**
- * Created by ellw on 04.02.15.
- */
 public class Rekursiv {
     private static boolean hlfsfkt(int i, int zahl) {
-        if (zahl == 0) {
-            return false;
-        }
         while (i * i < zahl) {
             hlfsfkt(++i, zahl);
         }
         if (i * i == zahl) {
             return true;
+        } else {
+            return false;
         }
-        return false;
-
     }
 
     public static boolean isQuadrat(int zahl) {
-        return hlfsfkt(0, zahl);
+        if (zahl == 0) {
+            return false;
+        } else {
+            return hlfsfkt(0, zahl);
+        }
     }
 
     public static void main(String[] args) {
         System.out.println(isQuadrat(0));
         System.out.println(isQuadrat(1));
-        System.out.println(isQuadrat(4));
         System.out.println(isQuadrat(3));
+        System.out.println(isQuadrat(4));
+        System.out.println(isQuadrat(25));
 
     }
 }
