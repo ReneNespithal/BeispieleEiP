ac8b76a9f7968becbf6242e5b102df745c90e65d
ellw <blu3tim3@gmail.com>
2015-02-04 14:53:18 +0100

    Update

--- src/testing/Fakultaet.java
+++ src/testing/Fakultaet.java
@@ -1,5 +1,18 @@
 package testing;
 
 public class Fakultaet {
-    public static
+    public static int fak(int i){
+        if(i==0){
+            return 1;
+        } else if(i ==1){
+            return 1;
+        }
+        return i*fak(i-1);
+    }
+
+    public static void main(String[] args){
+        System.out.println(fak(2));
+        System.out.println(fak(3));
+        System.out.println(fak(4));
+    }
 }
